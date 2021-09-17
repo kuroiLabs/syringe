c66921727f7bab9f7861b5617ad89951d36601a7
Jesse Rogers <jesse@jesserogers.net>
2021-09-16 22:46:34 -0700

    rename argument

--- src/injection-token/register-token.decorator.ts
+++ src/injection-token/register-token.decorator.ts
@@ -4,8 +4,8 @@ import { registerToken } from '../container'
  * @description Wraps the InjectionToken constructor to allow for code execution
  *  after the fact in order to auto-register the token after creation
  */
-export function RegisterToken<T extends {new(...args: any[]): {}}>(target: T) {
-  return class extends target {
+export function RegisterToken<T extends {new(...args: any[]): {}}>(_constructor: T) {
+  return class extends _constructor {
     constructor(...args: any[]) {
       super(...args)
       registerToken(<any>this)
