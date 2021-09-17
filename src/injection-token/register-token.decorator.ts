123c1aa696f330d21afb06a8f88935e140fc8927
Jesse Rogers <jesse@jesserogers.net>
2021-09-17 00:36:29 -0700

    formatting

--- src/injection-token/register-token.decorator.ts
+++ src/injection-token/register-token.decorator.ts
@@ -4,7 +4,7 @@ import { registerToken } from '../container'
  * @description Wraps the InjectionToken constructor to allow for code execution
  *  after the fact in order to auto-register the token after creation
  */
-export function RegisterToken<T extends {new(...args: any[]): {}}>(_constructor: T) {
+export function RegisterToken<T extends { new(...args: any[]): {} }>(_constructor: T) {
   return class extends _constructor {
     constructor(...args: any[]) {
       super(...args)
