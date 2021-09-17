3da9ad7645dd9915bb7a03c6d342e933752b93cb
Jesse Rogers <jesse@jesserogers.net>
2021-09-17 00:14:55 -0700

    add lifecycle hooks to classes decorated with @Injectable

--- src/injectable/injectable.decorator.ts
+++ src/injectable/injectable.decorator.ts
@@ -1,11 +1,12 @@
-import { registerToken } from '../container'
-import { InjectionToken, InjectionTokenConfig } from '../injection-token'
+import { destroyInstance } from '../container'
+import { InjectionToken } from '../injection-token/injection-token'
+import { InjectionTokenConfig } from '../injection-token/injection-token-config.interface'
 
 /**
  * @description Decorates a class as an injectable entity, generates an InjectionToken,
  *  and registers it to the Container
- * @see Container
  * @see InjectionToken
+ * @see Container
  */
 export const Injectable = (_token?: InjectionTokenConfig) => (_constructor: Function) => {
   const _key: string = _token && _token.name || _constructor.name
@@ -14,5 +15,16 @@ export const Injectable = (_token?: InjectionTokenConfig) => (_constructor: Func
     scope: _token && _token.scope,
     factory: () => _constructor
   })
-  registerToken(_injectionToken)
+  // add hidden injectable methods
+  _constructor.prototype.__initInjectable = () => {
+    if (_constructor.prototype.onInit) {
+      _constructor.prototype.onInit()
+    }
+  }
+  _constructor.prototype.__destroyInjectable = () => {
+    destroyInstance(_injectionToken.scope, _constructor)
+    if (_constructor.prototype.onDestroy) {
+      _constructor.prototype.onDestroy()
+    }
+  }
 }
