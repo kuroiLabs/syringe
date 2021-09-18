d996115dcc78d5682184529e14c22f9cc3cc46ac
Jesse Rogers <jesse@jesserogers.net>
2021-09-17 18:54:33 -0700

    reformat @Injectable decorator

--- src/injectable/injectable.decorator.ts
+++ src/injectable/injectable.decorator.ts
@@ -1,30 +1,33 @@
-import { destroyInstance } from '../container'
+import { Container } from '../container'
 import { InjectionToken } from '../injection-token/injection-token'
 import { InjectionTokenConfig } from '../injection-token/injection-token-config.interface'
 
 /**
  * @description Decorates a class as an injectable entity, generates an InjectionToken,
- *  and registers it to the Container
+ *  which automatically registters with the Container
  * @see InjectionToken
  * @see Container
  */
-export const Injectable = (_token?: InjectionTokenConfig) => (_constructor: Function) => {
-  const _key: string = _token && _token.name || _constructor.name
-  const _injectionToken: InjectionToken = new InjectionToken(_key, {
-    name: _key,
-    scope: _token && _token.scope,
-    factory: () => _constructor
-  })
-  // add hidden injectable methods
-  _constructor.prototype.__initInjectable = () => {
-    if (_constructor.prototype.onInit) {
-      _constructor.prototype.onInit()
+export const Injectable = (_token?: InjectionTokenConfig) => {
+  return function _injectableDecorator(_constructor: Function) {
+    const _key: string = _token && _token.name || _constructor.name
+    const _injectionToken: InjectionToken = new InjectionToken(_key, {
+      name: _key,
+      scope: _token && _token.scope,
+      factory: () => _constructor
+    })
+    // add hidden injectable lifecycle methods
+    _constructor.prototype.__initInjectable = () => {
+      if (_constructor.prototype.onInit) {
+        _constructor.prototype.onInit()
+      }
     }
-  }
-  _constructor.prototype.__destroyInjectable = () => {
-    destroyInstance(_injectionToken.scope, _constructor)
-    if (_constructor.prototype.onDestroy) {
-      _constructor.prototype.onDestroy()
+    _constructor.prototype.__destroyInjectable = () => {
+      const _scope: any = _injectionToken.scope || _constructor
+      Container.destroyInstance(_scope, _injectionToken)
+      if (_constructor.prototype.onDestroy) {
+        _constructor.prototype.onDestroy()
+      }
     }
   }
 }
