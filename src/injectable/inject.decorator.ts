9acc32c528f4637232ba4f627b3360e477905ec9
Jesse Rogers <jesse@jesserogers.net>
2021-09-17 18:54:07 -0700

    update @Inject decorator to scope injection to parent constructor context

--- src/injectable/inject.decorator.ts
+++ src/injectable/inject.decorator.ts
@@ -1,15 +1,17 @@
-import { addDependency, getToken, inject } from '../container'
+import { Container } from '../container'
 import { InjectionToken } from '../injection-token'
 
 /**
  * @description Decorates injected constructor parameters
  */
 export function Inject(_token: InjectionToken | Function) {
-  if (!(_token instanceof InjectionToken) && typeof _token === 'function') {
-    _token = getToken(_token.name)
-  }
-  return function (_target: Object, _key: string | symbol, _index: number) {
-    addDependency(_target, <InjectionToken>_token, _index)
-    return inject(_token.name)
+  const _injectionToken: InjectionToken = Container.getToken(_token.name)
+  return function _injectDecorator(
+    _target: Object,
+    _key: string | symbol,
+    _index: number
+  ) {
+    Container.addDependency(_target, _injectionToken, _index)
+    return Container.inject(_token.name, _injectionToken.scope || _target)
   }
 }
