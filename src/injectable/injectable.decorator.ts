8631a2e5e45a14aece79d370618a1070838740a6
Jesse Rogers <jesse@jesserogers.net>
2021-09-21 00:27:27 -0700

    remove lifecycle method setup from Injectable decorator

--- src/injectable/injectable.decorator.ts
+++ src/injectable/injectable.decorator.ts
@@ -1,4 +1,3 @@
-import { Container } from '../container'
 import { InjectionToken } from '../injection-token/injection-token'
 import { InjectionTokenConfig } from '../injection-token/injection-token-config.interface'
 
@@ -11,23 +10,10 @@ import { InjectionTokenConfig } from '../injection-token/injection-token-config.
 export const Injectable = (_token?: InjectionTokenConfig) => {
   return function _injectableDecorator(_constructor: Function) {
     const _key: string = _token && _token.name || _constructor.name
-    const _injectionToken: InjectionToken = new InjectionToken(_key, {
+    new InjectionToken(_key, {
       name: _key,
       scope: _token && _token.scope,
       factory: () => _constructor
     })
-    // add hidden injectable lifecycle methods
-    _constructor.prototype.__initInjectable = () => {
-      if (_constructor.prototype.onInit) {
-        _constructor.prototype.onInit()
-      }
-    }
-    _constructor.prototype.__destroyInjectable = () => {
-      const _scope: any = _injectionToken.scope || _constructor
-      Container.destroyInstance(_scope, _injectionToken)
-      if (_constructor.prototype.onDestroy) {
-        _constructor.prototype.onDestroy()
-      }
-    }
   }
 }
