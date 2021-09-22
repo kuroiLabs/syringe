29b01d11a5813ae06d4527c1dd3b9cd830a1731f
Jesse Rogers <jesse@jesserogers.net>
2021-09-21 23:23:17 -0700

    Update public-api.ts

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,7 +2,7 @@ import { Container } from './container'
 export const inject = Container.inject
 export const getToken = Container.getToken
 export { Container }
-export * from './injectable'
+export * from './decorators'
 export * from './injection-token'
 export * from './lifecycle'
 export * from './utils'
