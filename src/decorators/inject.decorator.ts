ab2916b1a1b3086ed2b8ac99974359943ebcf536
kuro <kuro@kuroi.io>
2022-02-12 21:29:42 -0800

    formatting

--- src/decorators/inject.decorator.ts
+++ src/decorators/inject.decorator.ts
@@ -5,15 +5,15 @@ import { InjectionToken } from '../injection-token'
  * @description Decorates injected constructor parameters to register dependencies
  */
 export function Inject(_token: InjectionToken | Function) {
-  return function _injectDecorator(
-    _target: Object,
-    _key: string | symbol,
-    _index: number
-  ) {
-    if (!_token) {
-      throw new ReferenceError(`${_target['name']} requested an InjectionToken (index [${_index}]) before it was initialized`)
-    }
-    const _injectionToken: InjectionToken = Container.getToken(_token.name)
-    Container.addDependency(_target, _injectionToken, _index)
-  }
+	return function _injectDecorator(
+		_target: Object,
+		_key: string | symbol,
+		_index: number
+	) {
+		if (!_token) {
+			throw new ReferenceError(`${_target['name']} requested an InjectionToken (index [${_index}]) before it was initialized`)
+		}
+		const _injectionToken: InjectionToken = Container.getToken(_token.name)
+		Container.addDependency(_target, _injectionToken, _index)
+	}
 }
