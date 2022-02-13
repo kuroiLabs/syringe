67b376a6e98355dd331dbcebc2b1a1ddf43596f0
kuro <kuro@kuroi.io>
2022-02-12 21:26:58 -0800

    use generic constructor type

--- src/injection-token/register-token.decorator.ts
+++ src/injection-token/register-token.decorator.ts
@@ -1,14 +1,15 @@
 import { Container } from '../container'
+import { Constructor } from '../utils'
 
 /**
  * @description Wraps the InjectionToken constructor to allow for code execution
  *  after the fact in order to auto-register the token after creation
  */
-export function RegisterToken<T extends { new(...args: any[]): {} }>(_constructor: T) {
-  return class extends _constructor {
-    constructor(...args: any[]) {
-      super(...args)
-      Container.registerToken(<any>this)
-    }
-  }
+export function RegisterToken<T extends Constructor>(Class: T) {
+	return class extends Class {
+		constructor(...args: any[]) {
+			super(...args)
+			Container.registerToken(<any>this)
+		}
+	}
 }
