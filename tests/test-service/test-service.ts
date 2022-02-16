4190b7cc9f7c2b1bf523cf095ad0cfeda6b068ef
kuro <kuro@kuroi.io>
2022-02-15 22:56:45 -0800

    extend abstract service

--- tests/test-service/test-service.ts
+++ tests/test-service/test-service.ts
@@ -2,37 +2,28 @@ import { Syringe } from '../../src'
 import { GENERATE_ID } from '../generator'
 import { InjectableClass } from '../injectable-class'
 import { OtherService } from '../other-service'
+import { BaseTestService } from './base-test-service'
 
 // singleton injectable
 @Syringe.Injectable({
-  scope: 'global'
+	scope: "global"
 })
-export class Service implements Syringe.OnInit, Syringe.OnDestroy {
+export class Service extends BaseTestService implements Syringe.OnInit, Syringe.OnDestroy {
 
-  private id: string
+	constructor(
+		@Syringe.Inject(InjectableClass) public dependency: InjectableClass,
+		@Syringe.Inject(OtherService) public childService: OtherService,
+		@Syringe.Inject(GENERATE_ID) _generator: () => string
+	) {
+		super(_generator())
+	}
 
-  constructor(
-    @Syringe.Inject(InjectableClass) public dependency: InjectableClass,
-    @Syringe.Inject(OtherService) public childService: OtherService,
-	@Syringe.Inject(GENERATE_ID) _generator: () => string
-  ) {
-    this.id = _generator()
-  }
+	public onInit() {
+		this.hello()
+	}
 
-  public onInit() {
-    this.hello()
-  }
-
-  public onDestroy() {
-    this.goodbye()
-  }
-
-  private hello() {
-    console.log('Service:::' + this.id, 'Hello!')
-  }
-
-  private goodbye() {
-    console.log('Service:::' + this.id, 'Goodbye!')
-  }
+	public onDestroy() {
+		this.goodbye()
+	}
 
 }
