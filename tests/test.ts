cf8fa380233b927a3f8f5b7fcdf266ae34ad4ad1
kuro <kuro@kuroi.io>
2022-02-15 22:57:51 -0800

    add abstract class provider

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,9 @@
 import { Syringe } from '../src'
 import { TestApp } from './app'
+import { BaseTestService, Service } from './test-service'
 
-Syringe.inject(TestApp)
+Syringe.inject(TestApp, null, [{
+	use: Service,
+	for: BaseTestService
+}])
 Syringe.Container.destroyAllInstances()
