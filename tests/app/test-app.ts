98fd17e17b3da26acd015628f095d597188a0366
kuro <kuro@kuroi.io>
2022-02-15 21:37:46 -0800

    formatting tests

--- tests/app/test-app.ts
+++ tests/app/test-app.ts
@@ -1,33 +1,33 @@
-import { Syringe } from '../../src'
-import { Extra } from '../decorator'
-import { Service } from '../test-service'
+import { Syringe } from "../../src"
+import { Extra } from "../decorator"
+import { Service } from "../test-service"
 
 @Syringe.Injectable({
-  scope: 'global'
+	scope: "global"
 })
 @Extra
 export class TestApp implements Syringe.OnInit {
 
-  private id: string
+	private id: string
 
-  constructor(@Syringe.Inject(Service) public service: Service) {
-    this.id = Syringe.generateId()
-  }
+	constructor(@Syringe.Inject(Service) public service: Service) {
+		this.id = Syringe.generateId()
+	}
 
-  public onInit(): void {
-    this.hello()
-  }
+	public onInit(): void {
+		this.hello()
+	}
 
-  public onDestroy(): void {
-    this.goodbye()
-  }
+	public onDestroy(): void {
+		this.goodbye()
+	}
 
-  private hello() {
-    console.log('TestApp:::' + this.id, 'Hello!')
-  }
+	private hello() {
+		console.log("TestApp:::" + this.id, "Hello!")
+	}
 
-  private goodbye() {
-    console.log('TestApp:::' + this.id, 'Goodbye!')
-  }
+	private goodbye() {
+		console.log("TestApp:::" + this.id, "Goodbye!")
+	}
 
 }
--- tests/other-service/other-service.ts
+++ tests/other-service/other-service.ts
@@ -3,31 +3,31 @@ import { Extra } from "../decorator";
 import { InjectableClass } from "../injectable-class";
 
 @Syringe.Injectable({
-  scope: 'global'
+	scope: "global"
 })
 @Extra
 export class OtherService implements Syringe.OnInit, Syringe.OnDestroy {
 
-  private id: string
+	private id: string
 
-  constructor(@Syringe.Inject(InjectableClass) private dependency: InjectableClass) {
-    this.id = Syringe.generateId()
-  }
+	constructor(@Syringe.Inject(InjectableClass) private dependency: InjectableClass) {
+		this.id = Syringe.generateId()
+	}
 
-  public onInit() {
-    this.hello()
-  }
+	public onInit() {
+		this.hello()
+	}
 
-  public onDestroy() {
-    this.goodbye()
-  }
+	public onDestroy() {
+		this.goodbye()
+	}
 
-  private hello(): void {
-    console.log(`OtherService::${this.id}`, 'Hello!')
-  }
+	private hello(): void {
+		console.log(`OtherService::${this.id}`, 'Hello!')
+	}
 
-  private goodbye(): void {
-    console.log(`OtherService::${this.id}`, 'Goodbye!')
-  }  
+	private goodbye(): void {
+		console.log(`OtherService::${this.id}`, 'Goodbye!')
+	}
 
 }
