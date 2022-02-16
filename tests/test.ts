8190f8ff7e9bf2bd73a66d645c9ed6b34e5f40e9
kuro <kuro@kuroi.io>
2022-02-15 23:43:29 -0800

    update test to include abstract service provider

--- tests/test.ts
+++ tests/test.ts
@@ -2,8 +2,10 @@ import { Syringe } from '../src'
 import { TestApp } from './app'
 import { BaseTestService, Service } from './test-service'
 
-Syringe.inject(TestApp, null, [{
-	use: Service,
-	for: BaseTestService
-}])
+Syringe.inject(TestApp, {
+	providers: [{
+		use: Service,
+		for: BaseTestService
+	}]
+})
 Syringe.Container.destroyAllInstances()
