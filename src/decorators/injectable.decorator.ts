6c63e9b407904a48cfe3e71be7298683f7d62877
kuro <kuro@kuroi.io>
2022-02-16 20:43:57 -0800

    type Injectable decorator argument as partial

--- src/decorators/injectable.decorator.ts
+++ src/decorators/injectable.decorator.ts
@@ -1,13 +1,15 @@
-import { InjectionToken, InjectionTokenConfig } from "../container"
+import { InjectionToken } from "../container"
 import { Constructor } from "../utils"
 
 /**
+ * @author kuro <kuro@kuroi.io>
+ * @namespace kuroi.io.Syringe.Decorators
  * @description Decorates a class as an injectable entity, generates an InjectionToken,
  *  which automatically registters with the Container
  * @see InjectionToken
  * @see Container
  */
-export function Injectable<T extends Constructor>(_token?: InjectionTokenConfig) {
+export function Injectable<T extends Constructor>(_token?: Partial<InjectionToken>) {
 	return function _injectableDecorator(Class: T) {
 		new InjectionToken(Class.name, {
 			scope: _token && _token.scope,
