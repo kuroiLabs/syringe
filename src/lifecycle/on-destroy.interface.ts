5788cba954432b489c0cb72668e0cd7b20ea9703
kuro <kuro@kuroi.io>
2022-02-16 20:44:07 -0800

    documentation/comments

--- src/lifecycle/on-destroy.interface.ts
+++ src/lifecycle/on-destroy.interface.ts
@@ -1,3 +1,10 @@
+/**
+ * @author kuro <kuro@kuroi.io>
+ * @namespace kuroi.io.Syringe.Lifecycle
+ * @description A lifecycle hook called when the Syringe Container
+ *   destroys the implementing object instance
+ */
 export interface OnDestroy {
+	/** Called when the Syringe Container destroys the implementing instance */
 	onDestroy(): void
 }
