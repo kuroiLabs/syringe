242125a2af55b7a57d40e5269e740f50afbdef0c
kuro <kuro@kuroi.io>
2022-02-17 23:15:01 -0800

    remove poorly written ID generator script
    was only useful for testing and there are much better solutions than this out there

--- src/utils/generate-id.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @author kuro <kuro@kuroi.io>
- * @namespace kuroi.io.Syringe.Utils
- */
-export function generateId(): string {
-	const _seed = Math.random() * 10 ** 16
-	return _seed.toString(32).substring(0, 10)
-}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from "./circular-dependency.error"
 export * from "./constants"
 export * from "./constructor.type"
-export * from "./generate-id"
 export * from "./null-injection-token.error"
