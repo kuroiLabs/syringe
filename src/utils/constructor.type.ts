ebfafdd3955c5861d118ac4dc3e6ec9e68be0032
kuro <kuro@kuroi.io>
2025-05-03 07:43:53 -0700

    improve Constructor type argument specificity

--- src/utils/constructor.type.ts
+++ src/utils/constructor.type.ts
@@ -2,4 +2,4 @@
  * @author kuro <kuro@kuroi.io>
  * @namespace kuroi.io.Syringe.Utils
  */
-export type Constructor<T = any> = { new (...args: any[]): T }
+export type Constructor<T = any, A extends any[] = []> = { new (...args: A[]): T }
