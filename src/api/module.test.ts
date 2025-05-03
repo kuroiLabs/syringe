9921b3493d68d955ef89d2fe8e3ee9a52ea25445
kuro <kuro@kuroi.io>
2025-05-03 16:26:33 -0700

    create Module

---
+++ src/api/module.test.ts
@@ -0,0 +1,43 @@
+import { describe, expect, test } from "vitest";
+import { inject } from "./inject";
+import { Injector } from "./injector";
+import { Module } from "./module";
+
+describe("Module", () => {
+	let module: Module;
+	const token: string = "test";
+
+	test("should provide itself", () => {
+		module = new Module();
+		expect(module.run(() => inject(Module))).toBe(module);
+	});
+
+	test("should use specified parent injector", () => {
+		const parent: Injector = new Injector({
+			providers: [{
+				token,
+				provide: () => token
+			}]
+		});
+
+		module = new Module({ injector: parent });
+
+		expect(module.injector.hasProvider(token)).toBe(false);
+		expect(() => module.inject(token)).not.toThrow();
+	});
+
+	test("should pull providers from imported modules", () => {
+		module = new Module({
+			imports: [
+				new Module({
+					providers: [{
+						token,
+						provide: ()	=> 100
+					}]
+				})
+			]
+		});
+
+		expect(module.inject(token)).toBe(100);
+	});
+});
---
+++ src/api/module.ts
@@ -0,0 +1,85 @@
+import { IProvider } from "../common";
+import { Destructible } from "../lifecycle";
+import { Injector } from "./injector";
+
+/**
+ * @author kuro <kuro@kuroi.io>
+ * @description Utility class for grouping and managing isolated and reusable Injection providers
+ */
+export class Module extends Destructible implements Module.Options {
+
+	public name: string;
+
+	public injector: Injector;
+
+	public imports: Module[];
+
+	public providers: IProvider<any>[];
+
+	public constructor();
+	public constructor(options: Partial<Module.Options>);
+
+	public constructor(options?: Partial<Module.Options>) {
+		super();
+
+		this.name = options?.name || 'SyringeModule';
+		this.imports = options?.imports || [];
+		this.providers = options?.providers ? [...options.providers] : [];
+
+		// ensure each Module provides itself
+		this.providers.push({
+			token: Module,
+			provide: () => this
+		});
+
+		this.injector = new Injector({
+			parent: options?.injector,
+			providers: this.getAllProviders()
+		});
+	}
+
+	public addProvider(...providers: IProvider[]): void {
+		this.providers.push(...providers);
+		this.injector.register(...providers);
+	}
+
+	public getAllProviders(): IProvider[] {
+		const providers: IProvider[] = [];
+
+		this.imports.forEach((imported: Module) => {
+			providers.push(...imported.getAllProviders());
+		});
+
+		// providers local to the module should come last so that 
+		// they take the highest priority in the Injector's provider map
+		providers.push(...this.providers);
+
+		return providers;
+	}
+
+	public inject<T = any>(token: any): T {
+		return this.injector.get(token);
+	}
+
+	public run<T = any>(context: () => T): T {
+		return this.injector.use(context);
+	}
+
+	public destroy(): void {
+		this.injector.destroy();
+	}
+
+}
+
+export namespace Module {
+	export interface Options {
+		/** The name of the module */
+		name: string;
+		/** Modules inherit the providers of their imported Modules */
+		imports: Module[];
+		/** Providers inherent to this Module */
+		providers: IProvider[];
+		/** The parent Injector to be used in this Module */
+		injector: Injector;
+	}
+}
