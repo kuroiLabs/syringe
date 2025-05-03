aaae7e58717b03ddc4399191b34f070e5b5c37a3
kuro <kuro@kuroi.io>
2025-05-03 16:24:01 -0700

    create Injector

---
+++ src/api/injector.test.ts
@@ -0,0 +1,141 @@
+import { beforeEach, describe, expect, MockInstance, test, vi } from "vitest";
+import { OnDestroy, OnInit } from "../lifecycle";
+import { NullProviderError } from "../utils";
+import { Injector } from "./injector";
+
+describe("Injector", () => {
+	let injector: Injector;
+	let token: string = "mytoken";
+	
+	describe("hasProvider", () => {
+		test("should return false if no provider", () => {
+			injector = new Injector();
+
+			expect(injector.hasProvider(token)).toBe(false);
+		});
+
+		test("should return true if existing provider", () => {
+			injector = new Injector({
+				providers: [{
+					token,
+					provide: () => token
+				}]
+			});
+
+			expect(injector.hasProvider(token)).toBe(true);
+		});
+	});
+
+	describe("register", () => {
+		test("should add providers at any point in time", () => {
+			injector = new Injector();
+
+			expect(injector.hasProvider(token)).toBe(false);
+
+			injector.register({ token, provide: () => token });
+
+			expect(injector.hasProvider(token)).toBe(true);
+		});
+	});
+
+	describe("use", () => {
+		test("should set the current Injector", () => {
+			injector = new Injector();
+
+			injector.use(() => {
+				expect(Injector.active).toBe(injector);
+			});
+		});
+	});
+
+	describe("get", () => {
+		let value: OnInit;
+
+		beforeEach(() => {
+			value = { onInit: vi.fn() };
+		});
+
+		test("should retrieve values from providers by token", () => {
+			injector = new Injector({
+				providers: [{
+					token,
+					provide: () => value
+				}]
+			});
+
+			expect(injector.get(token)).toBe(value);
+
+			// inject the same instance again
+			injector.get(token);
+			expect(value.onInit).toHaveBeenCalledOnce();
+		});
+
+		test("should recursively traverse the Injector tree to find a provider", () => {
+			injector = new Injector({
+				providers: [{
+					token,
+					provide: () => value
+				}]
+			});
+
+			for (let i: number = 0; i < 3; i++)
+				injector = new Injector({ parent: injector });
+
+			expect(injector.get(token)).toBe(value);
+		});
+
+		test("should throw a NullProviderError if no provider", () => {
+			injector = new Injector();
+
+			expect(() => injector.get(token)).toThrow(NullProviderError);
+		});
+
+		test("should not throw an error provided a not-found value", () => {
+			const notFoundValue: string = "i tried";
+
+			injector = new Injector();
+
+			expect(injector.get(token, notFoundValue)).toBe(notFoundValue);
+		});
+	});
+
+	describe("destroy", () => {
+		let destructible: OnDestroy;
+
+		beforeEach(() => {
+			destructible = { onDestroy: vi.fn() };
+			injector = new Injector({
+				providers: [{
+					token,
+					provide: () => destructible
+				}]
+			});
+		});
+
+		test("should invoke OnDestroy for any OnDestroy implementations", () => {
+			injector.get(token);
+			injector.destroy();
+
+			expect(destructible.onDestroy).toHaveBeenCalled();
+		});
+
+		test("should clear all providers", () => {
+			expect(injector.hasProvider(token)).toBe(true);
+			injector.destroy();
+			expect(injector.hasProvider(token)).toBe(false);
+		});
+
+		test("should destroy any child injectors", () => {
+			const parent: Injector = new Injector();
+			const child: Injector = new Injector({ parent });
+
+			const parentDestroy: MockInstance<() => void> = vi.spyOn(parent, "destroy");
+			const childDestroy: MockInstance<() => void> = vi.spyOn(child, "destroy");
+
+			parent.destroy();
+
+			expect(parentDestroy).toHaveBeenCalled();
+			expect(childDestroy).toHaveBeenCalled();
+		});
+	});
+});
---
+++ src/api/injector.ts
@@ -0,0 +1,145 @@
+import { Destructible, isOnDestroy, isOnInit } from "../lifecycle";
+import { NullProviderError } from "../utils";
+import { IProvider } from "../common/provider";
+
+/**
+ * @author kuro <kuro@kuroi.io>
+ * @description The Injector is the foundational component driving the Syringe system.
+ * 	Injectors exist in a hierarchical structure and recursively check their parents for providers
+ * 	before throwing an error and giving up.
+ */
+export class Injector extends Destructible {
+
+	/** The currently active `Injector` instance */
+	public static active: Injector | null = null;
+
+	/** The parent of this `Injector` */
+	public readonly parent?: Injector;
+
+	/** Child instances of this `Injector` */
+	protected readonly children: Set<Injector> = new Set();
+
+	/** Providers accessible in this `Injector` */
+	protected readonly providers: Map<any, IProvider> = new Map([
+		[Injector, {
+			token: Injector,
+			provide: () => this
+		}]
+	]);
+
+	/** Cached values returned from this `Injector`'s providers */
+	protected readonly values: Map<any, any> = new Map();
+
+	public constructor();
+	public constructor(options: Partial<Injector.Options>);
+	public constructor(options?: Partial<Injector.Options>) {
+		super();
+
+		this.parent = options?.parent || Injector.active;
+
+		if (this.parent === this)
+			this.parent = null;
+		else
+			this.parent?.children.add(this);
+
+		if (options?.providers)
+			this.register(...options.providers);
+	}
+
+	/**
+	 * Checks if the Injector has a registered Provider for a given token
+	 * @param token The token representing the Provider
+	 * @returns `true` if the Injector has a Provider for the token
+	 */
+	public hasProvider(token: any): boolean {
+		return this.providers.has(token);
+	}
+
+	/**
+	 * Adds a Provider to the Injector
+	 * @param provider The provider to register
+	 */
+	public register(...providers: IProvider[]): void {
+		providers.forEach((provider: IProvider) => {
+			this.providers.set(provider.token, provider);
+		})
+	}
+
+	/**
+	 * Use the context of an `Injector` instance to run logic with access to the provider scope
+	 * 	of the `Injector`.
+	 * @param context Logic to run in the context of this `Injector`
+	 */
+	public use<T = any>(context: () => T): T {
+		Injector.active = this;
+
+		const ctx: T = context();
+
+		Injector.active = null;
+
+		return ctx;
+	}
+
+	public get<T = any>(token: any): T;
+	public get<T = any>(token: any, notFoundValue: T): T;
+
+	/**
+	 * Extracts a value for a given Provider token
+	 * @param token The token representing the Provider
+	 * @param notFoundValue An optional fallback value if no available Provider
+	 */
+	public get<T = any>(token: any, notFoundValue?: T): T {
+		let injector: Injector = this;
+
+		while (injector) {
+			if (!injector.hasProvider(token)) {
+				injector = injector.parent;
+				continue;
+			}
+
+			let value: T;
+
+			if (!injector.values.has(token)) {
+				value = injector.use(() => injector.providers.get(token).provide());
+
+				if (isOnInit(value))
+					value.onInit();
+
+				injector.values.set(token, value);
+			} else
+				value = injector.values.get(token);
+
+			return value;
+		}
+
+		if (notFoundValue === undefined)
+			throw new NullProviderError(token);
+
+		return notFoundValue;
+	}
+
+	public destroy(): void {
+		super.destroy();
+
+		this.values.forEach((value: any) => {
+			if (isOnDestroy(value))
+				value.onDestroy();
+		});
+
+		this.providers.clear();
+		this.values.clear();
+
+		this.children.forEach(c => c.destroy());
+		this.parent?.children.delete(this);
+	}
+
+}
+
+export namespace Injector {
+
+	export interface Options {
+		parent: Injector;
+		providers: IProvider[];
+	}
+
+}
