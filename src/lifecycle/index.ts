c40eb86d2ff148c59acb5c7f5891412a09009cb1
kuro <kuro@kuroi.io>
2025-05-03 16:21:37 -0700

    Update index.ts

--- src/lifecycle/index.ts
+++ src/lifecycle/index.ts
@@ -1,2 +1,5 @@
-export * from './on-init.interface'
-export * from './on-destroy.interface'
+export * from "./destructible";
+export * from "./is-on-destroy.guard";
+export * from "./is-on-init.guard";
+export * from "./on-destroy.interface";
+export * from "./on-init.interface";
